125a610b418fabee17c29884fd0da88ebf5df540
nehaba4n-lgtm <nehaba4n@gmail.com>
2026-05-05 23:29:32 +0530

    Add files via upload

---
+++ largestnumber.py
@@ -0,0 +1,13 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+7
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+print("The largest number is:", largest)
+
