abd6e362f3de630b182bc58d1e54b2dbb183711a
nehaba4n-lgtm <nehaba4n@gmail.com>
2026-05-06 10:29:44 +0530

    Update arthematicoperations.py

--- arthematicoperations.py
+++ arthematicoperations.py
@@ -7,3 +7,13 @@ print("Division of a and b ",a/b)
 print("Remainder of a and b ",a%b)
 print("Exponent of a and b ",a**b)
 print("Floar division of a and b ",a//b)
+output 
+Enter a value7
+Enter b value8
+Addition of a and b  15
+Subtraction of a and b  -1
+Multiplication of a and b  56
+Division of a and b  0.875
+Remainder of a and b  7
+Exponent of a and b  5764801
+Floar division of a and b  0
